8e40680c30eae4e6d171df7a002ed76b3940db80
dharanesh <dharaneshk2003@gmail.com>
2024-10-11 23:28:42 +0530

    added 2 more projects

--- src/Components/Projects/Project.jsx
+++ src/Components/Projects/Project.jsx
@@ -11,6 +11,8 @@ import yelpcamp from "../../assets/projects/yelpcamp.png";
 import cricapi from "../../assets/projects/cricapi.jpg";
 import translang from "../../assets/projects/translang.png";
 import gemini from "../../assets/projects/gemini.png";
+import url from "../../assets/projects/url.jpg";
+import business from "../../assets/projects/business.jpg";
 import "./Project.css";
 
 export default function Project() {
@@ -34,7 +36,7 @@ export default function Project() {
                 <Typography gutterBottom variant="h5" component="div">
                   Yelp-Camp
                 </Typography>
-                <Typography variant="body2" color="text.secondary" sx={{textAlign:'justify'}} >
+                <Typography variant="body2" color="text.secondary" sx={{ textAlign: 'justify' }} >
                   YelpCamp is a web application to connect enthusiasts by sharing ,exploring campgrounds, user reviews, ratings, and images,forms a community platform for camping enthusiasts.
                 </Typography>
               </CardContent>
@@ -55,10 +57,10 @@ export default function Project() {
               />
               <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
-                  Score-Cric hi
+                  Score-Cric
                 </Typography>
-                <Typography variant="body2" color="text.secondary" sx={{textAlign:'justify'}} >
-                  CricAPI is a website that integrates cricket data from various sources, offering fast responses for accessing real-time scores, match information, including empowering interaction with cricket enthusiasts.
+                <Typography variant="body2" color="text.secondary" sx={{ textAlign: 'justify' }} >
+                  CricAPI is a website that integrates cricket data from various sources, offering fast responses for accessing real-time scores, match information, including empowering interaction.
                 </Typography>
               </CardContent>
               <CardActions>
@@ -73,7 +75,7 @@ export default function Project() {
           </Stack>
         </div>
         <div className="project-container">
-          <Stack direction={isMobile ? "column" : "row"} spacing={{ xs: 10, lg: 25 } } className="stack-row-p">
+          <Stack direction={isMobile ? "column" : "row"} spacing={{ xs: 10, lg: 25 }} className="stack-row-p">
             <Card sx={{ ...cardDimensions, border: "4px solid black", p: 2, borderRadius: 10 }}>
               <CardMedia
                 sx={{ height: { xs: 180, lg: 215 } }}
@@ -84,8 +86,8 @@ export default function Project() {
                 <Typography gutterBottom variant="h5" component="div">
                   Trans-Lang
                 </Typography>
-                <Typography variant="body2" color="text.secondary" sx={{textAlign:'justify'}} >
-                TransLang Project enables seamless response multilingual communication using translation API&apos;s ,which overcomes language barriers, and promoting global understanding.
+                <Typography variant="body2" color="text.secondary" sx={{ textAlign: 'justify' }} >
+                  TransLang Project enables seamless response multilingual communication using translation API&apos;s ,which overcomes language barriers, and promoting global understanding.
                 </Typography>
               </CardContent>
               <CardActions>
@@ -107,8 +109,8 @@ export default function Project() {
                 <Typography gutterBottom variant="h5" component="div">
                   Gemini-Clone
                 </Typography>
-                <Typography variant="body2" color="text.secondary" sx={{textAlign:'justify'}} >
-                  Gemini Project clone aims to revolutionize digital and  identity management with front-end technology, offering decentralized solutions for secure and transparent data handling across diverse industries globally.
+                <Typography variant="body2" color="text.secondary" sx={{ textAlign: 'justify' }} >
+                  Gemini Project clone aims to revolutionize digital and  identity management with front-end technology, offering decentralized solutions for secure.
                 </Typography>
               </CardContent>
               <CardActions>
@@ -117,6 +119,56 @@ export default function Project() {
                 </Button>
                 <Button size="small" sx={{ color: "black" }}>
                   <a href='https://gemini-tony.netlify.app/' style={{ color: "black", textDecoration: "none" }}>Live Demo</a>
+                </Button>
+              </CardActions>
+            </Card>
+          </Stack>
+        </div>
+        <div className="project-container">
+          <Stack direction={isMobile ? "column" : "row"} spacing={{ xs: 10, lg: 25 }} className="stack-row-p">
+            <Card sx={{ ...cardDimensions, border: "4px solid black", p: 2, borderRadius: 10 }}>
+              <CardMedia
+                sx={{ height: { xs: 180, lg: 215 } }}
+                image={business}
+                title="Business-Directory"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Business-Directory
+                </Typography>
+                <Typography variant="body2" color="text.secondary" sx={{ textAlign: 'justify' }} >
+                The Business Directory app helps users find and explore local businesses, offering easy search, categorization, and detailed listings. It simplifies business discovery.
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" sx={{ color: "black" }}>
+                  <a href='https://github.com/dharaneshk2003/business-directory' style={{ color: "black", textDecoration: "none" }}>Github</a>
+                </Button>
+                <Button size="small" sx={{ color: "black" }}>
+                  <a href='https://drive.google.com/file/d/1mgquMsPnPhCxY244ii_YYfXW-qEAySFo/view' style={{ color: "black", textDecoration: "none" }}>Drive Link</a>
+                </Button>
+              </CardActions>
+            </Card>
+            <Card sx={{ ...cardDimensions, border: "4px solid black", p: 2, borderRadius: 10 }}>
+              <CardMedia
+                sx={{ height: { xs: 160, lg: 215 } }}
+                image={url}
+                title="URL-Trimrr"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  URL-Trimrr
+                </Typography>
+                <Typography variant="body2" color="text.secondary" sx={{ textAlign: 'justify' }} >
+                  The URL Shortener project simplifies long URLs into compact, shareable links while tracking performance with analytics. It offers a user-friendly interface for creating, managing
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" sx={{ color: "black" }}>
+                  <a href='https://github.com/dharaneshk2003/url_shortner' style={{ color: "black", textDecoration: "none" }}>Github</a>
+                </Button>
+                <Button size="small" sx={{ color: "black" }}>
+                  <a href='https://url-shortner-tau-seven.vercel.app/' style={{ color: "black", textDecoration: "none" }}>Live Demo</a>
                 </Button>
               </CardActions>
             </Card>
